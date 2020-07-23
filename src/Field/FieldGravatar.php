263f613ccd913d9d66e847039be759c26a9d8f5f
Charlie Page <charliepage88@gmail.com>
2020-07-23 19:06:33 -0400

    bug fixing

--- src/Field/FieldGravatar.php
+++ src/Field/FieldGravatar.php
@@ -10,6 +10,25 @@ class FieldGravatar extends FieldType
   use EmptyColumnMigrations;
 
   /**
+  * @var array
+  */
+  public $defaultSettings = [
+    'options' => [
+      'is_sortable'        => false,
+      'is_searchable'      => false,
+      'is_required_create' => false,
+      'is_required_edit'   => false
+    ],
+    'action_rules' => [
+      'index'  => true,
+      'create' => false,
+      'edit'   => false,
+      'show'   => true,
+      'search' => true
+    ]
+  ];
+
+  /**
   * @var boolean
   */
   public $shouldNotSetData = true;
